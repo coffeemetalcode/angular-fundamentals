8852d44ebcabe4ff73115660533f5b79979d9919
dbstocker <dstocker@notesettersinc.com>
2019-09-03 17:52:25 -0400

    updates

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -11,4 +11,22 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class EventThumbnailComponent {
   @Input() event: any;
 
+  getStartTimeClass() {
+    // const isEarlyStart = this.event && this.event.time === '8:00';
+    // return {
+    //   green: isEarlyStart, bold: isEarlyStart
+    // };
+
+    if (this.event && this.event.time === '8:00') { return 'green bold'; }
+    return '';
+  }
+
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === '8:00') {
+      return { color: '#003300', 'font-weight': 'bold'};
+    } else {
+      return {};
+    }
+  }
+
 }
